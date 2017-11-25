a261b65916045588b13b3a6f13095dec503e53ab
Xav83130 <xgesset@free.fr>
2017-11-25 01:19:23 +0100

    Little changes interface

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 #kivy.require('1.10.0')
 
+import serial
+import time
+import multiprocessing
+
 import kivy
 
 from kivy.app import App
@@ -7,7 +11,9 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
-from kivy.graphics import Line
+
+SERIAL_PORT = '/dev/cu.chusbserialfd130'
+SERIAL_BAUDRATE = 115200
 
 class Desktop(Widget):
     pass
