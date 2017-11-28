9bc99ff9d51d1502aff0c5dbed163c0e59ba5b98
Xav83130 <xgesset@free.fr>
2017-11-28 11:27:27 +0100

    Merge branch 'serial-widgets'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.logger import Logger
+<<<<<<< HEAD
 
+=======
+from kivy.uix.behaviors import ToggleButtonBehavior
+#from kivy.uix.boxlayout import BoxLayout
+#from kivy.uix.textinput import TextInput
+#from kivy.uix.image import Image
+from kivy.properties import ObjectProperty
+>>>>>>> serial-widgets
 
 class Desktop(Widget):
     pass
@@ -20,9 +28,10 @@ class ArmApp(App):
         return Desktop()
 
     def idle(self): # change d'etat le Togglebutton ligne 64 du .kv
+        #self.idle = bool(self.root.ids.idle.state)
         # si je suis connecté au port serie
-        # alors le bouton change d'etat 'state: down'
-        pass
+        # alors le bouton change d'etat "state: 'down' "
+        #pass
 
     def connect(self): # connection au port serie
         Logger.info("Connecting to serial device: {} with baudrate: {}".format(
@@ -44,6 +53,9 @@ class ArmApp(App):
             )
             popup.open()
 
+    def readSerial(self): #lis le retour du port serie
+        return self.serial.readline()
+
     def disconnect(self): # deconnection au port serie
         print("Je me déconnecte du port serie !!")
         self.serial = serial.Serial = None
@@ -55,9 +67,37 @@ class ArmApp(App):
         print("Je retire l'alarme")
         self._send_command("$X")
 
+    def rst_grbl(self): # Reset GRBL
+        print("Je retire reset GRBL")
+        self._send_command("ctrl-x")
+
+    def cycle_start(self): # demarre cycle à voir si c'est utile
+        print("Je demarre un cycle")
+        self._send_command("~")
+
+    def feed_hold(self):
+        print("Feed_hold ??")
+        self._send_command("!")
+
     def rst_xyz(self):  # Reset XYZ
         print("Reset XYZ remise a zero XYZ")
-        self._send_command("")
+        self._send_command("G92X0Y0Z0")        #commande gcode a revoir
+
+    def rst_x(self):  # Reset X
+        print("Reset X remise a zero X")
+        self._send_command("G10P0L20X0")        #commande gcode a revoir
+
+    def rst_y(self):  # Reset X
+        print("Reset Y remise a zero Y")
+        self._send_command("G92Y0")        #commande gcode a revoir
+
+    def rst_z(self):  # Reset Z
+        print("Reset Z remise a zero Z")
+        self._send_command("G92Z0")        #commande gcode a revoir
+
+    def home(self):  # Retour X0 Y0 Z0
+        print("Retour position X0Y0Z0")
+        self._send_command("G30")
 
     def x_move_pos(self): # move X+. Il faut remplacer la valeur de X(1) par la valeur du curseur "pas".
         print("mouvement de X en positif")
